470098f010bb8a9c21d1ff68ad0920ba5670aaa9
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-12 16:00:08 -0300

    Desarrollando función mostrarAvenger

--- js/script.js
+++ js/script.js
@@ -6,7 +6,26 @@ const getConnection = () => {
     const ts = Date.now()
     const hash = MD5(ts + privateKey + publicKey) //Javascript no tiene un metodo md5, por lo que descargué el algoritmo que lo genera y este archivo lo llamé primero que el js
     const URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
-    fetch(URL).then(response => console.log(response))
+    fetch(URL)
+        .then(response => response.json())
+        .then(response => {
+            response.data.results.forEach(e => {
+                mostrarAvenger(e)
+            })
+        })
 };
+
+const mostrarAvenger = e => {
+    
+    const imagen = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`
+    const avenger = `
+    <div>  
+        <h3>${e.name}</h3>
+        <img src="${imagen}">
+        <p>${e.description}</p>
+    </div>
+    `
+    contenido.insertAdjacentHTML('beforeEnd', avenger) //inserts a text as HTML, into a specified position
+}
 
 getConnection()
