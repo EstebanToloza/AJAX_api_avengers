a04305bb938f5acb49f23763e5e781e5ecc22e1f
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-12 15:03:21 -0300

    Relacionando con Api - funciona!

--- js/script.js
+++ js/script.js
@@ -3,10 +3,14 @@ const publicKey = "b6a2ec37c446eab7659a6103b9868690"
 const contenido = document.querySelector("#contenido")
 
 const getConnection = () => {
-    const ts = 1
-    const hash = "f3e7b7eff63dd8fae6e070ce2dd407e9"
-    const URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey${publicKey}&hash=${hash}`
+    const ts = Date.now()
+    const hash = MD5(ts + privateKey + publicKey)
+    const URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
     fetch(URL).then(response => console.log(response))
-}
+    /* .then(response => response.json())
+    .then(response => {
+        console.log(response)
+    }); */
+};
 
 getConnection()
