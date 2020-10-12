951eb23ae1c2e6312ec57418769d3df78d8452a4
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-12 15:35:40 -0300

    Creando gitignore

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-const privateKey = "8bae64f0b81256c18461d21f79ab1bbb3ea84897"
-const publicKey = "b6a2ec37c446eab7659a6103b9868690"
+//Variables globales
 const contenido = document.querySelector("#contenido")
 
+//Estableciendo conección con API de Marvel
 const getConnection = () => {
     const ts = Date.now()
-    const hash = MD5(ts + privateKey + publicKey)
+    const hash = MD5(ts + privateKey + publicKey) //Javascript no tiene un metodo md5, por lo que descargué el algoritmo que lo genera y este archivo lo llamé primero que el js
     const URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
     fetch(URL).then(response => console.log(response))
-    /* .then(response => response.json())
-    .then(response => {
-        console.log(response)
-    }); */
 };
 
 getConnection()
