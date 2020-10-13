0c77fe89998a256b1c140561f5ded758a2e4e7e2
EstebanToloza <estebanisaiastoloza@gmail.com>
2020-10-13 17:18:21 -0300

    Se integra información de API al front. Se crea hoja de estilos.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,41 @@
 const contenido = document.querySelector("#contenido")
 
 //Estableciendo conección con API de Marvel
-const getConnection = () => {
+/* 
+const cargarAvengers = async function(){
+
+    try {
+
+        const ts = Date.now() 
+        const hash = MD5(ts + privateKey + publicKey)
+        const URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
+        const response = await fetch(URL)
+        const fichas = await response.json()
+
+        //console.log(fichas)
+
+        const avengers = fichas.map( () => {
+            const heroe = document.querySelector(".row").cloneNode(true)
+
+            const imagen = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`
+
+            heroe.querySelector('img').src = imagen
+            heroe.querySelector('.card-title').innerText = item.name
+            heroe.querySelector('.card-text').innerText = item.description
+
+            return heroe
+        })
+
+    } catch {
+        alert("Problemas!")
+    }
+} */
+
+
+
+
+
+const traerAvenger = () => {
     const ts = Date.now()
     const hash = MD5(ts + privateKey + publicKey) //Javascript no tiene un metodo md5, por lo que descargué el algoritmo que lo genera y este archivo lo llamé primero que el js
     const URL = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`
@@ -13,19 +47,22 @@ const getConnection = () => {
                 mostrarAvenger(e)
             })
         })
-};
 
-const mostrarAvenger = e => {
-    
-    const imagen = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`
-    const avenger = `
-    <div>  
-        <h3>${e.name}</h3>
-        <img src="${imagen}">
-        <p>${e.description}</p>
-    </div>
-    `
-    contenido.insertAdjacentHTML('beforeEnd', avenger) //inserts a text as HTML, into a specified position
+        const mostrarAvenger = e => {
+        const imagen = `${e.thumbnail.path}/portrait_uncanny.${e.thumbnail.extension}`
+        const avenger = 
+            `
+            <div class="tarjeta">
+                <h3 class="nombre">${e.name}</h3>  
+                <div class="avenger-img">
+                    <img src="${imagen}" class="">
+                    <p class="texto">${e.description}</p>
+                </div>
+            </div>
+            `  
+        contenido.insertAdjacentHTML('beforeEnd', avenger) //inserts a text as HTML, into a specified position
+    }
 }
 
-getConnection()
+//*col-lg-4 col-md-6 col-sm-12*/
+traerAvenger()
